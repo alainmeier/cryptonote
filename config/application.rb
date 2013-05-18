88597df6eb990616e3132c3edc838f5adb31a88c
Alain Meier <me@alainmeyer.com>
2013-05-17 21:14:37 -0700

    Don't initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Cryptonote
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
   end
 end
