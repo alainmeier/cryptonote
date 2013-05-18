314653e5ebf3b2246c7b765fecdb848b8d66d71b
Alain Meier <me@alainmeyer.com>
2013-05-17 22:01:13 -0700

    Add basic message creation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,27 @@
 class MessagesController < ApplicationController
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.create(message_params)
+    redirect_to root_path
   end
 
   def show
   end
 
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:content, 
+                                      :location, 
+                                      :password, 
+                                      :latitude, 
+                                      :longitude)
+    end
 end
