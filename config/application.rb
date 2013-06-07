93a48078f71ffc52478c39f8980bbb92c46e83e0
Alain Meier <me@alainmeyer.com>
2013-06-07 04:58:14 -0700

    Forgot to load middleware

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(:default, Rails.env)
 
 module Cryptonote
   class Application < Rails::Application
+    # Middleware for redirecting www
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.middleware.use "WwwDitcher"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
