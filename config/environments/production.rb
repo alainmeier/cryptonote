3db910c30d92f1348a08729eaa97197bd7555f07
Alain Meier <me@alainmeier.com>
2014-12-31 18:09:11 -0800

    Another try at fixing asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Cryptonote::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.precompile += %w( errors.css )
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
